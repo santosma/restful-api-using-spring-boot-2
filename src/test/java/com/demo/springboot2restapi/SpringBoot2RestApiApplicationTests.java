bb1dc36e35399eea4bf22309d6a708cfe43ba5e5
M.Santos <miguel.santos.student@gmail.com>
2022-01-30 20:11:07 -0500

    First commit

---
+++ src/test/java/com/demo/springboot2restapi/SpringBoot2RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springboot2restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
